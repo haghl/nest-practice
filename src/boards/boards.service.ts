d2e30e9200394d6d82f0bd220668ddec41242f4a
DongGyu Won <wdg980813@gmail.com>
2023-08-07 02:32:38 +0900

    연습

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -11,8 +11,12 @@ import { User } from 'src/auth/user.entity'
 export class BoardsService {
   constructor(private boardRepository: BoardRepository) {}
 
-  async getAllBoards(): Promise<Board[]> {
-    return await this.boardRepository.find()
+  async getAllBoards(user: User): Promise<Board[]> {
+    const query = this.boardRepository.createQueryBuilder('board')
+    query.where('board.userId = :userId', { userId: user.id })
+    const boards = await query.getMany()
+
+    return boards
   }
   async getBoardById(id: number): Promise<Board> {
     const found = await this.boardRepository.findOneBy({ id })
