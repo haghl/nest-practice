e48e2b9c8174658abe8280975e85b61f97af7ddd
WonDongGyu <wdg980813@gmail.com>
2023-08-06 14:05:38 +0900

    추가 완료

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -2,13 +2,13 @@ import { IsString, Matches, MaxLength, MinLength } from 'class-validator'
 
 export class AuthCredentialDto {
   @IsString()
-  @MinLength(4)
+  @MinLength(3)
   @MaxLength(20)
   userName: string
 
   @IsString()
   @MinLength(8)
   @MaxLength(20)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/, { message: '영문, 숫자, 특수기호를 조합하여 비밀번호를 만들어주세요.' })
+  @Matches(/^(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/, { message: '영문, 숫자, 특수기호를 조합하여 비밀번호를 만들어주세요.' })
   password: string
 }
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -14,8 +14,8 @@ export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get()
-  getAllBoards(): Promise<Board[]> {
-    return this.boardsService.getAllBoards()
+  getAllBoards(@GetUser() user: User): Promise<Board[]> {
+    return this.boardsService.getAllBoards(user)
   }
 
   @Get('/:id')
